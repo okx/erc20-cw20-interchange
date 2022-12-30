dc43e5ba5e4dd9a419f89460330f91c27ba11f87
巴扎黑 <1298170866@qq.com>
2022-12-30 15:19:33 +0800

    Update WASMdeploy.js
    add note

--- scripts/WASMdeploy.js
+++ scripts/WASMdeploy.js
@@ -1,5 +1,4 @@
 var wasm = require("cosmwasm");
-var crypto = require("@cosmjs/crypto");
 var amino = require("@cosmjs/amino");
 var encoding =require("@cosmjs/encoding");
 var fs = require("fs");
@@ -18,9 +17,12 @@ async function deployWASMContract(evmContract) {
 
     let filedata =fs.readFileSync('./WASMContracts/target/wasm32-unknown-unknown/release/cw_erc20.wasm')
 
+    //update the contract
     var result = await cwclient.upload(alice.address,filedata,{"amount":wasm.parseCoins("200000000000000000wei"),"gas":"20000000"})
 
     var initMsg = {"name":"testERC","symbol":"TRC", "decimals": 18,"evm_contract" :evmContract};
+
+    //init the contract
     var res2 = await cwclient.instantiate(alice.address, result.codeId, initMsg, "hello world", {"amount":wasm.parseCoins("200000000000000000wei"),"gas":"20000000"},{"funds":[{"denom":"okt","amount":"100000000000000000000"}],"admin":alice.address})
 
     return res2;
