084ceabcd07fb96fe28f234160be3f8b8fca2ae7
巴扎黑 <1298170866@qq.com>
2023-03-21 10:16:38 +0800

    add OKBC

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -4,20 +4,19 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
-contract Bridge is Initializable{
-
-    address internal constant MODULE_ADDRESS = 0xc63cf6c8E1f3DF41085E9d8Af49584dae1432b4f;
+contract Bridge is Initializable {
+    address internal constant MODULE_ADDRESS =
+        0xc63cf6c8E1f3DF41085E9d8Af49584dae1432b4f;
     string internal wasmContractAddress;
     address internal evmContractAddress;
 
     event Initialize(string wasmContractAddress, address evmContractAddress);
-    event __OKCSendToWasm(string wasmAddr, string recipient, uint256 amount);
+    event __OKBCSendToWasm(string wasmAddr, string recipient, uint256 amount);
 
     function initialize(
         string calldata _wasmContractAddress,
         address _token
-    ) public initializer{
-
+    ) public initializer {
         wasmContractAddress = _wasmContractAddress;
         evmContractAddress = _token;
 
@@ -25,15 +24,19 @@ contract Bridge is Initializable{
     }
 
     function mintERC20(
-        string calldata caller, 
+        string calldata caller,
         address recipient,
         uint256 amount
-    ) 
-    external
-    returns (bool) 
-    {
-        require(msg.sender == MODULE_ADDRESS, "Only Wasm specified address can call");
-        require(keccak256(abi.encodePacked(caller)) == keccak256(abi.encodePacked(wasmContractAddress)), "Only specified wasm contract can call");
+    ) external returns (bool) {
+        require(
+            msg.sender == MODULE_ADDRESS,
+            "Only Wasm specified address can call"
+        );
+        require(
+            keccak256(abi.encodePacked(caller)) ==
+                keccak256(abi.encodePacked(wasmContractAddress)),
+            "Only specified wasm contract can call"
+        );
 
         IERC20(evmContractAddress).transfer(recipient, amount);
         return true;
@@ -44,9 +47,12 @@ contract Bridge is Initializable{
      * @param recipient it must be "ex" address
      */
     function send_to_wasm(string memory recipient, uint128 amount) public {
-        
-        IERC20(evmContractAddress).transferFrom(msg.sender, address(this), uint256(amount));
+        IERC20(evmContractAddress).transferFrom(
+            msg.sender,
+            address(this),
+            uint256(amount)
+        );
 
-        emit __OKCSendToWasm(wasmContractAddress, recipient, amount);
+        emit __OKBCSendToWasm(wasmContractAddress, recipient, amount);
     }
 }
